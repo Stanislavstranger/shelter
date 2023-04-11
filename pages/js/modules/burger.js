2b34a39f60838ef3335abea6f7b2bd8b5fa5d39b
Stanislav Goloborodov <stanislavstranger@gmail.com>
2023-04-11 14:41:47 +0300

    feat: add menu-burger on pets page

--- pages/js/modules/burger.js
+++ pages/js/modules/burger.js
@@ -1,11 +1,12 @@
 const burgerButton = document.querySelector('#burger__button');
+const menuList = document.querySelector('.menu__list');
+const headerMenu = document.querySelector('.header__menu');
+const header = document.querySelector('.header');
+const headerContainer = document.querySelector('.header__container');
+const logoActive = document.querySelector('.logo');
 
-const burgerFunctionality = () => {
+const burgerToggle = () => {
     if (burgerButton) {
-        const headerMenu = document.querySelector('.header__menu');
-        const header = document.querySelector('.header');
-        const headerContainer = document.querySelector('.header__container');
-        const logoActive = document.querySelector('.logo');
         burgerButton.addEventListener('click', function (e) {
             document.body.classList.toggle('_lock');
             burgerButton.classList.toggle('_active');
@@ -17,4 +18,18 @@ const burgerFunctionality = () => {
     }
 }
 
-burgerFunctionality();
+const burgerClose = () => {
+    if (menuList) {
+        menuList.addEventListener('click', function (e) {
+            document.body.classList.remove('_lock');
+            burgerButton.classList.remove('_active');
+            headerMenu.classList.remove('_active');
+            header.classList.remove('_active');
+            headerContainer.classList.remove('_active');
+            logoActive.classList.remove('_active');
+        });
+    }
+}
+
+burgerToggle();
+burgerClose();
